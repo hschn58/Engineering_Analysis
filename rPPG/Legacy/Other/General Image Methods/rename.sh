53633d38366c8b949d564f0538a6e97ca94c07f2
Henry <hschn58@gmail.com>
2025-09-21 19:36:29 -0500

    Reorganization

--- rPPG/Legacy/Other/General Image Methods/rename.sh
+++
@@ -1,8 +0,0 @@
-for file in "$directory"/*_trimmed.mp4; do
-    # Extract the base filename without the "_trimmed"
-    new_name="${file/_trimmed/}"
-    
-    # Rename the file
-    mv "$file" "$new_name"
-    echo "Renamed $file to $new_name"
-done
